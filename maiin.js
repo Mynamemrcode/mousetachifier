7c6db5a319dbf01de8d0b70d79a7a7c281bdf9c5
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-07-16 11:44:05 -0700

    Add files via upload

---
+++ maiin.js
@@ -0,0 +1,14 @@
+function preload() {
+
+}
+
+function setup() {
+    Canvas = canvas.createCanvas(500, 250);
+    canvas.position(590,200);
+}
+function draw() {
+
+}
+function savingmachine() {
+    save("Moustache.png");
+}
