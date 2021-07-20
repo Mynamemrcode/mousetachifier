9f25a5e763a37c208ae9c4c3bbaca9f301f7611e
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-07-20 10:53:36 -0700

    Add files via upload

--- maiin.js
+++ maiin.js
@@ -3,12 +3,28 @@ function preload() {
 }
 
 function setup() {
-    Canvas = canvas.createCanvas(500, 250);
-    canvas.position(590,200);
+    Canvas = createCanvas(500, 250);
+    Canvas.position(590,200);
+    Video = createCapture(VIDEO);
+Video.size(500,250);
+Video.hide();
+s = ml5.poseNet(Video, Load);
+s.on('pose', gotNoses);
 }
+function gotNoses(result) {
+    if (result.length >0) {
+        console.log(result);
+        console.log(result[0].pose.nose.x);
+        console.log(result[0].pose.nose.y);
+    }
+    }
 function draw() {
-
+    image(Video, 0,0,500,250);
 }
 function savingmachine() {
     save("Moustache.png");
+}
+
+function Load() {
+    console.log("poseNet connection Successful");
 }
