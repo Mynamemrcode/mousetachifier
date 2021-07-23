db1af323a907a0dfb0202e1d11bb90c437d81d43
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-07-23 12:23:47 -0700

    Add files via upload

--- maiin.js
+++ maiin.js
@@ -1,5 +1,7 @@
+nosy = 0;
+nosx = 0;
 function preload() {
-
+    masker = loadImage("https://i.postimg.cc/J0SqTgvS/mask.png");
 }
 
 function setup() {
@@ -15,11 +17,14 @@ function gotNoses(result) {
     if (result.length >0) {
         console.log(result);
         console.log(result[0].pose.nose.x);
-        console.log(result[0].pose.nose.y);
+        console.log(result[0].pose.nose.y);   
+        nosy = result[0].pose.nose.y + 20 - 40 - 10 + 5;
+        nosx = result[0].pose.nose.x -50 - 20 - 30;
     }
     }
 function draw() {
     image(Video, 0,0,500,250);
+    image(masker,nosx,nosy, 200,100)
 }
 function savingmachine() {
     save("Moustache.png");
